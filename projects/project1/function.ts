5aa500a85d47ac1fc861c6c69a02837fb77f736e
sultanshah76 <139227068+sultanshah76@users.noreply.github.com>
2023-11-04 21:49:39 +0500

    complete 2 project

---
+++ projects/project1/function.ts
@@ -0,0 +1,8 @@
+export function addition(num1:number,num2:number) {
+     return num1+num2}
+     export function subtraction(num1:number,num2:number) {
+        return num1-num2}
+        export function multiplication(num1:number,num2:number) {
+            return num1*num2}
+            export function division(num1:number,num2:number) {
+                return num1/num2}
