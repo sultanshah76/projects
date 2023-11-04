9cd0a95bc7f1aa690d3959a3a9c6291ac1410677
sultanshah76 <139227068+sultanshah76@users.noreply.github.com>
2023-11-04 21:12:45 +0500

    Initial commit

--- practise/checkin.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function logWarriors(greeting, ...names) {
-    console.log(`${greeting}, ${name}!`);
-}
-const warriors = ["Cathay Williams", "Lozen", "Nzinga"];
-console.log(logWarriors("Hello", ...warriors));
-const birthYears = [1844, 1840, 1583];
-logWarriors("Born in", ...birthYears);
